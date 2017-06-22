4b8c14f2597d2e793652cdc2e09e9a6e6254b393
Albert Yi <ayi@desk.com>
2017-06-21 17:39:46 -0700

    fix eval_batch.sh script

--- scripts/eval_batch.sh
+++ scripts/eval_batch.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-find ~/tf-data-multi/images -name '00*' | head -n 10 | python3 slimception/classify_image.py
+DATA_HOME_DIR=${DATA_HOME_DIR:-~/tf-data-multi}
+
+find ${DATA_HOME_DIR}/images | shuf | head -n 5 | python3 slimception/classify_image.py
