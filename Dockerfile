8415d9d1fac57ce9053ad8e285790794303aa498
Albert Yi <ayi@desk.com>
2017-07-18 10:56:11 -0700

    switch to test

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 # you should run this with nvidia-docker
 
-FROM danbooru/tensorflow:latest
+FROM tensorflow/tensorflow:latest-gpu-py3
 MAINTAINER Albert Yi "r888888888@gmail.com"
 RUN apt-get update -y
 COPY . /app
 WORKDIR /app
 RUN pip3 install -r requirements.txt
 ENTRYPOINT ["python3"]
-CMD ["web/ccs.py"]
+CMD ["web/test.py"]
--- web/test.py
+++ web/test.py
@@ -1,4 +1,16 @@
 from flask import Flask
+from flask import request, render_template, redirect, url_for
+from werkzeug.utils import secure_filename
+from dotenv import load_dotenv
+import json
+import re
+import numpy as np
+import tensorflow as tf
+# from slimception.datasets import characters
+# from slimception.preprocessing import preprocessing_factory
+# from slimception.nets import nets_factory
+# from slimception.datasets import dataset_factory
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,3 +19,4 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
+
